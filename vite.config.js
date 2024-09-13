ab0c84b863a61b07a89658173b67bf1b57dd7aaa
KaloyanKostov <kostov.kaloyan44@gmail.com>
2024-09-13 18:28:50 +0300

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/Github-Profile-Finder/", // Ensure this matches your repo name
+  build: {
+    outDir: "dist", // This should be the default, but ensure it's set
+  },
+});
